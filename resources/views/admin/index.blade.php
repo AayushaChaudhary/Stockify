94f9c98592fdb83460042212b48bb3e89e3c03c9
sujan kumal <sujanmaskey987@gmail.com>
2023-07-25 10:48:47 +0545

    layout and backend changes done

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -34,10 +34,10 @@
             </div>
             <div class="avatar-sm">
                 <span class="avatar-title bg-light text-primary rounded-3">
-                    <i class="ri-shopping-cart-2-line font-size-24"></i>  
+                    <i class="ri-shopping-cart-2-line font-size-24"></i>
                 </span>
             </div>
-        </div>                                            
+        </div>
     </div><!-- end cardbody -->
 </div><!-- end card -->
 </div><!-- end col -->
@@ -53,10 +53,10 @@
             </div>
             <div class="avatar-sm">
                 <span class="avatar-title bg-light text-success rounded-3">
-                    <i class="mdi mdi-currency-usd font-size-24"></i>  
+                    <i class="mdi mdi-currency-usd font-size-24"></i>
                 </span>
             </div>
-        </div>                                              
+        </div>
     </div><!-- end cardbody -->
 </div><!-- end card -->
 </div><!-- end col -->
@@ -72,10 +72,10 @@
             </div>
             <div class="avatar-sm">
                 <span class="avatar-title bg-light text-primary rounded-3">
-                    <i class="ri-user-3-line font-size-24"></i>  
+                    <i class="ri-user-3-line font-size-24"></i>
                 </span>
             </div>
-        </div>                                              
+        </div>
     </div><!-- end cardbody -->
 </div><!-- end card -->
 </div><!-- end col -->
@@ -91,17 +91,17 @@
             </div>
             <div class="avatar-sm">
                 <span class="avatar-title bg-light text-success rounded-3">
-                    <i class="mdi mdi-currency-btc font-size-24"></i>  
+                    <i class="mdi mdi-currency-btc font-size-24"></i>
                 </span>
             </div>
-        </div>                                              
+        </div>
     </div><!-- end cardbody -->
 </div><!-- end card -->
 </div><!-- end col -->
 </div><!-- end row -->
 
 {{-- < class="row">
- 
+
 
 <div class="row">
 <div class="col-xl-12">
@@ -111,7 +111,7 @@
             <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="mdi mdi-dots-vertical"></i>
             </a>
-         
+
         </div>
 
         <h4 class="card-title mb-4">Latest Transactions</h4>
@@ -243,7 +243,7 @@
 <!-- end col -->
 
 
- 
+
 
 
 </div> --}}
@@ -280,129 +280,80 @@
                 <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="mdi mdi-dots-vertical"></i>
                 </a>
-             
+
             </div>
-    
-            <h4 class="card-title text-bold mb-4">Latest Transactions</h4>
-    
+
+            <h4 class="card-title text-bold mb-4">Latest Purchase</h4>
+
             <div class="table-responsive">
                 <table class="table table-centered mb-0 align-middle table-hover table-nowrap">
                     <thead class="table-light">
                         <tr>
-                            <th>Name</th>
-                            <th>Position</th>
-                            <th>Status</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th style="width: 120px;">Salary</th>
+                            <th>Supplier</th>
+                            <th>Product</th>
+                            <th>quantity</th>
+                            <th>Price Per Unit</th>
+                            <th>Total Price</th>
                         </tr>
                     </thead><!-- end thead -->
                     <tbody>
+                        @foreach($transactions as $transaction)
                         <tr>
-                            <td><h6 class="mb-0">Charles Casey</h6></td>
-                            <td>Web Developer</td>
+                            <td><h6 class="mb-0">{{ $transaction->supplier->name }}</h6></td>
+                            <td>{{ $transaction->product->name }}</td>
                             <td>
-                                <div class="font-size-13"><i class="ri-checkbox-blank-circle-fill font-size-10 text-success align-middle me-2"></i>Active</div>
+                               {{ $transaction->buying_qty }}
                             </td>
                             <td>
-                                23
+                                {{ $transaction->unit_price }}
                             </td>
                             <td>
-                                04 Apr, 2021
+                                {{ $transaction->buying_price }}
                             </td>
-                            <td>$42,450</td>
                         </tr>
-                         <!-- end -->
-                         <tr>
-                            <td><h6 class="mb-0">Alex Adams</h6></td>
-                            <td>Python Developer</td>
-                            <td>
-                                <div class="font-size-13"><i class="ri-checkbox-blank-circle-fill font-size-10 text-warning align-middle me-2"></i>Deactive</div>
-                            </td>
-                            <td>
-                                28
-                            </td>
-                            <td>
-                                01 Aug, 2021
-                            </td>
-                            <td>$25,060</td>
+                        @endforeach
+                    </tbody><!-- end tbody -->
+                </table> <!-- end table -->
+            </div>
+        </div><!-- end card -->
+    </div><!-- end card -->
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xl-12 pt-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="dropdown float-end">
+                <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="mdi mdi-dots-vertical"></i>
+                </a>
+
+            </div>
+
+            <h4 class="card-title text-bold mb-4">Latest Sales</h4>
+
+            <div class="table-responsive">
+                <table class="table table-centered mb-0 align-middle table-hover table-nowrap">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Customer</th>
+                            <th>Product</th>
+                            <th>Quantity </th>
+                            <th>Price per unit</th>
+                            <th>Total Price</th>
                         </tr>
-                         <!-- end -->
-                         <tr>
-                            <td><h6 class="mb-0">Prezy Kelsey</h6></td>
-                            <td>Senior Javascript Developer</td>
-                            <td>
-                                <div class="font-size-13"><i class="ri-checkbox-blank-circle-fill font-size-10 text-success align-middle me-2"></i>Active</div>
-                            </td>
-                            <td>
-                                35
-                            </td>
-                            <td>
-                                15 Jun, 2021
-                            </td>
-                            <td>$59,350</td>
+                    </thead><!-- end thead -->
+                    <tbody>
+                        @foreach($sales as $key => $item)
+                        <tr>
+                            <td> {{ $item['payment']['customer']['name'] }} </td>
+                            <td> #{{ $item["invoice_details"][0]["product"]["name"] }} </td>
+                            <td> #{{ $item['invoice_details'][0]['selling_qty'] }} </td>
+                            <td> {{ $item['invoice_details'][0]['unit_price']  }} </td>
+                            <td>  $ {{ $item['payment']['total_amount'] }} </td>
                         </tr>
-                         <!-- end -->
-                         <tr>
-                            <td><h6 class="mb-0">Ruhi Fancher</h6></td>
-                            <td>React Developer</td>
-                            <td>
-                                <div class="font-size-13"><i class="ri-checkbox-blank-circle-fill font-size-10 text-success align-middle me-2"></i>Active</div>
-                            </td>
-                            <td>
-                                25
-                            </td>
-                            <td>
-                                01 March, 2021
-                            </td>
-                            <td>$23,700</td>
-                        </tr>
-                         <!-- end -->
-                         <tr>
-                            <td><h6 class="mb-0">Juliet Pineda</h6></td>
-                            <td>Senior Web Designer</td>
-                            <td>
-                                <div class="font-size-13"><i class="ri-checkbox-blank-circle-fill font-size-10 text-success align-middle me-2"></i>Active</div>
-                            </td>
-                            <td>
-                                38
-                            </td>
-                            <td>
-                                01 Jan, 2021
-                            </td>
-                            <td>$69,185</td>
-                        </tr>
-                         <!-- end -->
-                         <tr>
-                            <td><h6 class="mb-0">Den Simpson</h6></td>
-                            <td>Web Designer</td>
-                            <td>
-                                <div class="font-size-13"><i class="ri-checkbox-blank-circle-fill font-size-10 text-warning align-middle me-2"></i>Deactive</div>
-                            </td>
-                            <td>
-                                21
-                            </td>
-                            <td>
-                                01 Sep, 2021
-                            </td>
-                            <td>$37,845</td>
-                        </tr>
-                         <!-- end -->
-                         <tr>
-                            <td><h6 class="mb-0">Mahek Torres</h6></td>
-                            <td>Senior Laravel Developer</td>
-                            <td>
-                                <div class="font-size-13"><i class="ri-checkbox-blank-circle-fill font-size-10 text-success align-middle me-2"></i>Active</div>
-                            </td>
-                            <td>
-                                32
-                            </td>
-                            <td>
-                                20 May, 2021
-                            </td>
-                            <td>$55,100</td>
-                        </tr>
-                         <!-- end -->
+                        @endforeach
                     </tbody><!-- end tbody -->
                 </table> <!-- end table -->
             </div>
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,17 @@ use App\Http\Controllers\Pos\InvoiceController;
 use App\Http\Controllers\Pos\StockController;
 use App\Models\Category;
 use App\Models\Customer;
+use App\Models\Invoice;
+use App\Models\Payment;
 use App\Models\Product;
+use App\Models\Purchase;
 use App\Models\Supplier;
 use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 });
- 
+
 
 Route::controller(DemoController::class)->group(function () {
     Route::get('/about', 'Index')->name('about.page')->middleware('check');
@@ -35,7 +38,7 @@ Route::controller(DemoController::class)->group(function () {
 
 
 
- // Admin All Route 
+ // Admin All Route
 Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
     Route::get('/admin/profile', 'Profile')->name('admin.profile');
@@ -44,24 +47,24 @@ Route::controller(AdminController::class)->group(function () {
 
     Route::get('/change/password', 'ChangePassword')->name('change.password');
     Route::post('/update/password', 'UpdatePassword')->name('update.password');
-     
+
 });
 
 
- // Supplier All Route 
+ // Supplier All Route
 Route::controller(SupplierController::class)->group(function () {
-    Route::get('/supplier/all', 'SupplierAll')->name('supplier.all'); 
-    Route::get('/supplier/add', 'SupplierAdd')->name('supplier.add'); 
+    Route::get('/supplier/all', 'SupplierAll')->name('supplier.all');
+    Route::get('/supplier/add', 'SupplierAdd')->name('supplier.add');
     Route::post('/supplier/store', 'SupplierStore')->name('supplier.store');
-    Route::get('/supplier/edit/{id}', 'SupplierEdit')->name('supplier.edit'); 
+    Route::get('/supplier/edit/{id}', 'SupplierEdit')->name('supplier.edit');
     Route::post('/supplier/update', 'SupplierUpdate')->name('supplier.update');
     Route::get('/supplier/delete/{id}', 'SupplierDelete')->name('supplier.delete');
 });
 
 
-// Customer All Route 
+// Customer All Route
 Route::controller(CustomerController::class)->group(function () {
-    Route::get('/customer/all', 'CustomerAll')->name('customer.all'); 
+    Route::get('/customer/all', 'CustomerAll')->name('customer.all');
     Route::get('/customer/add', 'CustomerAdd')->name('customer.add');
     Route::post('/customer/store', 'CustomerStore')->name('customer.store');
     Route::get('/customer/edit/{id}', 'CustomerEdit')->name('customer.edit');
@@ -82,50 +85,50 @@ Route::controller(CustomerController::class)->group(function () {
        Route::get('/customer/wise/report', 'CustomerWiseReport')->name('customer.wise.report');
        Route::get('/customer/wise/credit/report', 'CustomerWiseCreditReport')->name('customer.wise.credit.report');
        Route::get('/customer/wise/paid/report', 'CustomerWisePaidReport')->name('customer.wise.paid.report');
-     
+
 });
 
 
-// Unit All Route 
+// Unit All Route
 Route::controller(UnitController::class)->group(function () {
-    Route::get('/unit/all', 'UnitAll')->name('unit.all'); 
+    Route::get('/unit/all', 'UnitAll')->name('unit.all');
     Route::get('/unit/add', 'UnitAdd')->name('unit.add');
     Route::post('/unit/store', 'UnitStore')->name('unit.store');
     Route::get('/unit/edit/{id}', 'UnitEdit')->name('unit.edit');
     Route::post('/unit/update', 'UnitUpdate')->name('unit.update');
     Route::get('/unit/delete/{id}', 'UnitDelete')->name('unit.delete');
-     
+
 });
 
 
-// Category All Route 
+// Category All Route
 Route::controller(CategoryController::class)->group(function () {
-    Route::get('/category/all', 'CategoryAll')->name('category.all'); 
+    Route::get('/category/all', 'CategoryAll')->name('category.all');
     Route::get('/category/add', 'CategoryAdd')->name('category.add');
     Route::post('/category/store', 'CategoryStore')->name('category.store');
     Route::get('/category/edit/{id}', 'CategoryEdit')->name('category.edit');
     Route::post('/category/update', 'CategoryUpdate')->name('category.update');
     Route::get('/category/delete/{id}', 'CategoryDelete')->name('category.delete');
-     
+
 });
 
 
-// Product All Route 
+// Product All Route
 Route::controller(ProductController::class)->group(function () {
-    Route::get('/product/all', 'ProductAll')->name('product.all'); 
+    Route::get('/product/all', 'ProductAll')->name('product.all');
     Route::get('/product/add', 'ProductAdd')->name('product.add');
     Route::post('/product/store', 'ProductStore')->name('product.store');
     Route::get('/product/edit/{id}', 'ProductEdit')->name('product.edit');
     Route::post('/product/update', 'ProductUpdate')->name('product.update');
     Route::get('/product/delete/{id}', 'ProductDelete')->name('product.delete');
-     
+
 });
 
 
-  
-// Purchase All Route 
+
+// Purchase All Route
 Route::controller(PurchaseController::class)->group(function () {
-    Route::get('/purchase/all', 'PurchaseAll')->name('purchase.all'); 
+    Route::get('/purchase/all', 'PurchaseAll')->name('purchase.all');
     Route::get('/purchase/add', 'PurchaseAdd')->name('purchase.add');
     Route::post('/purchase/store', 'PurchaseStore')->name('purchase.store');
     Route::get('/purchase/delete/{id}', 'PurchaseDelete')->name('purchase.delete');
@@ -134,13 +137,13 @@ Route::controller(PurchaseController::class)->group(function () {
 
     Route::get('/daily/purchase/report', 'DailyPurchaseReport')->name('daily.purchase.report');
     Route::get('/daily/purchase/pdf', 'DailyPurchasePdf')->name('daily.purchase.pdf');
-     
+
 });
 
 
-// Invoice All Route 
+// Invoice All Route
 Route::controller(InvoiceController::class)->group(function () {
-    Route::get('/invoice/all', 'InvoiceAll')->name('invoice.all'); 
+    Route::get('/invoice/all', 'InvoiceAll')->name('invoice.all');
     Route::get('/invoice/add', 'invoiceAdd')->name('invoice.add');
     Route::post('/invoice/store', 'InvoiceStore')->name('invoice.store');
 
@@ -154,24 +157,24 @@ Route::controller(InvoiceController::class)->group(function () {
 
     Route::get('/daily/invoice/report', 'DailyInvoiceReport')->name('daily.invoice.report');
     Route::get('/daily/invoice/pdf', 'DailyInvoicePdf')->name('daily.invoice.pdf');
-    
-     
+
+
 });
 
 
 
 
 
-// Stock All Route 
+// Stock All Route
 Route::controller(StockController::class)->group(function () {
     Route::get('/stock/report', 'StockReport')->name('stock.report');
-    Route::get('/stock/report/pdf', 'StockReportPdf')->name('stock.report.pdf'); 
+    Route::get('/stock/report/pdf', 'StockReportPdf')->name('stock.report.pdf');
 
-    Route::get('/stock/supplier/wise', 'StockSupplierWise')->name('stock.supplier.wise'); 
+    Route::get('/stock/supplier/wise', 'StockSupplierWise')->name('stock.supplier.wise');
     Route::get('/supplier/wise/pdf', 'SupplierWisePdf')->name('supplier.wise.pdf');
     Route::get('/product/wise/pdf', 'ProductWisePdf')->name('product.wise.pdf');
 
- 
+
 });
 
 
@@ -181,12 +184,12 @@ Route::controller(StockController::class)->group(function () {
 
 
 
-// Default All Route 
+// Default All Route
 Route::controller(DefaultController::class)->group(function () {
-    Route::get('/get-category', 'GetCategory')->name('get-category'); 
-    Route::get('/get-product', 'GetProduct')->name('get-product'); 
-    Route::get('/check-product', 'GetStock')->name('check-product-stock'); 
-     
+    Route::get('/get-category', 'GetCategory')->name('get-category');
+    Route::get('/get-product', 'GetProduct')->name('get-product');
+    Route::get('/check-product', 'GetStock')->name('check-product-stock');
+
 });
 
 
@@ -200,7 +203,10 @@ Route::get('/dashboard', function (ThisWeekPurchaseChart $chart, StockByCategory
     $counts['suppliers'] = Supplier::count();
     $counts['products'] = Product::count();
     $counts['categories'] = Category::count();
-    return view('admin.index', ['chart' => $chart->build(), 'stockChart' => $stockChart->build(), 'counts' => $counts]);
+    $transactions = Purchase::with('supplier', 'product')->where("status", 1)->latest()->limit(10)->get();
+    $sales = Invoice::with('invoice_details.product', 'payment')->where('status','1')->latest()->limit(10)->get();
+    // return $sales;
+    return view('admin.index', ['chart' => $chart->build(), 'stockChart' => $stockChart->build(), 'counts' => $counts, 'transactions'=>$transactions, 'sales' => $sales]);
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
